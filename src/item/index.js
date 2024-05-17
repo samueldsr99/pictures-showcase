0c245940d86812fde9d4136ea51e9293a0200246
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-05-17 08:51:05 +0200

    feat: add texts

--- src/item/index.js
+++ src/item/index.js
@@ -13,6 +13,7 @@ import gsap from "gsap";
 
 import vertexShader from "./shaders/index.vert";
 import fragmentShader from "./shaders/index.frag";
+import { FontLoader, TextGeometry } from "three/examples/jsm/Addons.js";
 
 export default class Item extends Mesh {
   constructor({ camera, imagePath, title, description, width = 5 }) {
@@ -32,6 +33,7 @@ export default class Item extends Mesh {
     const texture = await new TextureLoader().loadAsync(this.imagePath);
     this._uniforms = {
       uTexture: { value: texture },
+      uIsInFront: { value: this._isInFront },
     };
 
     const geometry = new PlaneGeometry(1, 1, 32, 32);
@@ -47,7 +49,7 @@ export default class Item extends Mesh {
 
     const mesh = new Mesh(geometry, material);
     this._mesh = mesh;
-    this._initialPosition = mesh.position.clone();
+    this._initialPosition = this.position.clone();
 
     const meshWidth = this.width;
     const meshHeight = this.width / ratio;
@@ -55,6 +57,44 @@ export default class Item extends Mesh {
     mesh.scale.set(meshWidth, meshHeight, 1);
 
     this.add(mesh);
+
+    this._initTexts();
+  }
+
+  async _initTexts() {
+    // Load fonts
+    const loader = new FontLoader();
+    const pradaFont = await loader.loadAsync("src/fonts/prada.json");
+
+    // Title text
+    const titleGeometry = new TextGeometry(this.title, {
+      font: pradaFont,
+      size: 0.2,
+      depth: 0.07,
+    });
+    const titleMaterial = new MeshBasicMaterial({
+      color: 0x333333,
+    });
+    const titleMesh = new Mesh(titleGeometry, titleMaterial);
+    this._titleMesh = titleMesh;
+    titleMesh.position.set(-2, -2, 0.1);
+    titleMesh.visible = false;
+    this.add(titleMesh);
+
+    // Description text
+    const descriptionGeometry = new TextGeometry(this.description, {
+      font: pradaFont,
+      size: 0.08,
+      depth: 0.01,
+    });
+    const descriptionMaterial = new MeshBasicMaterial({
+      color: 0x333333,
+    });
+    const descriptionMesh = new Mesh(descriptionGeometry, descriptionMaterial);
+    this._descriptionMesh = descriptionMesh;
+    descriptionMesh.position.set(-2, -2.3, 0.1);
+    descriptionMesh.visible = false;
+    this.add(descriptionMesh);
   }
 
   /**
@@ -66,8 +106,12 @@ export default class Item extends Mesh {
       this.resetPosition();
     }
 
+    this._titleMesh.visible = true;
+    this._descriptionMesh.visible = true;
     this._isInFront = true;
-    gsap.to(this._mesh.position, {
+    console.log("bring to front", this._isInFront);
+
+    gsap.to(this.position, {
       z: 32,
       duration: 0.5,
     });
@@ -75,7 +119,10 @@ export default class Item extends Mesh {
 
   resetPosition() {
     this._isInFront = false;
-    gsap.to(this._mesh.position, {
+    this._titleMesh.visible = false;
+    this._descriptionMesh.visible = false;
+
+    gsap.to(this.position, {
       x: this._initialPosition.x,
       y: this._initialPosition.y,
       z: this._initialPosition.z,
@@ -83,5 +130,14 @@ export default class Item extends Mesh {
     });
   }
 
-  update() {}
+  update() {
+    if (!this._uniforms) {
+      return;
+    }
+
+    this._uniforms.uIsInFront.value = this._isInFront;
+    if (this._mesh) {
+      this._mesh.material.uniforms = this._uniforms;
+    }
+  }
 }
--- src/item/shaders/index.frag
+++ src/item/shaders/index.frag
@@ -1,6 +1,8 @@
 uniform sampler2D uTexture;
+uniform bool uIsInFront;
 
 varying vec2 vUv;
+varying vec3 vPosition;
 
 void main() {
     gl_FragColor = texture2D(uTexture, vUv);
